c67731893f43865c0f4a3aff51cbc1f06a564346
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-03-24 16:02:47 +0100

    added server for static files

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var static = require('node-static');
+
+var staticFiles = new(static.Server)('./static');
+
+http.createServer(function(request, response) {
+    request.addListener('end', function () {
+        // Serve files!
+        staticFiles.serve(request, response, function (err, result) {
+             if (err) {
+                 console.error("Error serving " + request.url + " - " + err.message);
+                 response.writeHead(err.status, err.headers);
+                 response.end();
+             }
+         });
+    }).resume();
+}).listen(8888);
