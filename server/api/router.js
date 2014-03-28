1869a1a071c1c6de289fae804463af1d1ab1552a
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-03-28 04:16:58 +0100

    router: errors bubble up the chains of routers

--- server/api/router.js
+++ server/api/router.js
@@ -6,19 +6,22 @@ var url = require("url");
 module.exports = Router;
 
 function Router(options) {
+  //interpret the options
   if(options === undefined) options = {};
   var unknownRoute = options.unknownRoute || defaultUnknownRoute;
   var unknownMethod = options.unknownMethod || defaultUnknownMethod;
   var baseUrl = options.baseUrl || "";
 
+  //this router is used in order to parse the urls
   var router = new RoutesRouter();
 
-  function handleRequest(req, res, parentOpts) {
+  //handleRequest handles the actual requests and will be returned by this function
+  var handleRequest = function (req, res, parentOpts) {
     var route = router.match(url.parse(req.url).pathname)
 
     if (!route) {
       res.statusCode = 404;
-      return unknownRoute(req, res, parentOpts);
+      return handleRequest.unknownRoute(req, res, parentOpts);
     }
 
     var opts = {
@@ -34,32 +37,44 @@ function Router(options) {
       }
       opts.splats = parentOpts.splats.concat(opts.splats);
     }
+
     return route.fn(req, res, opts);
   }
-
+  //set the error handlers
+  handleRequest.unknownMethod = unknownMethod;
+  handleRequest.unknownRoute = unknownRoute;
+  //expose a possibility to add routes
   handleRequest.addRoute = function(uri, fn) {
     if (typeof fn === "object") {
-      fn = wrapMethodSelector(fn, unknownMethod);
+      fn = wrapMethodSelector(fn, handleRequest.unknownMethod);
     }
     router.addRoute(uri, fn);
     return this;
   };
+  //expose a possibility to add other routers as sub routers
   handleRequest.addChildRouter = function(uri, childRouter) {
     if(uri[uri.length - 1] != '/') {
       uri += "(?:/*)?";
     } else {
       uri += "*?";
     }
+    if(childRouter.unknownRoute === defaultUnknownRoute) {
+      childRouter.unknownRoute = handleRequest.unknownRoute;
+    }
+    if(childRouter.unknownMethod === defaultUnknownMethod) {
+      childRouter.unknownMethod = handleRequest.unknownMethod;
+    }
     var wrappedRouter = wrapChildRouter(childRouter);
     router.addRoute(uri, wrappedRouter);
     return this;
-  }
+  };
 
   return handleRequest;
 }
 
+//handles the selection of a function based on the HTTP method
 function wrapMethodSelector(routes, unknownRouteCallback) {
-  function handleUnknownRoute(req, res) {
+  function handleUnknownRoute(req, res, opts) {
     res.statusCode = 405;
     var acceptedMethods = Object.keys(routes);
     res.setHeader("Accept", acceptedMethods.join(', '));
@@ -72,8 +87,9 @@ function wrapMethodSelector(routes, unknownRouteCallback) {
   }
 }
 
+//returns a wrapped child router which is able to handle the urls correctly
 function wrapChildRouter(router) {
-  return function childRouter(req, res, opts) {
+  return function adjustUrlForChildRouter(req, res, opts) {
     var remainingUrl = opts.splats.pop();
     if(remainingUrl == undefined) remainingUrl = "/";
     remainingUrl = "/" + remainingUrl;
