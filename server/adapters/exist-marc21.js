637edaafbe42da95299de8fd7f47d5239ac7ae09
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-04-09 05:28:36 +0200

    adapter for Marc21: parse the result

--- server/adapters/exist-marc21.js
+++ server/adapters/exist-marc21.js
@@ -1,25 +1,59 @@
+"use strict";
+var util = require("util");
 var http = require("http");
+var concat = require("concat-stream");
+var et = require("elementtree");
 
 var apiEndpoint = "http://localhost:8080/exist/rest//db/od/marc21_search.xq";
 
-var url =  apiEndpoint + "?limit=1";
+var url =  apiEndpoint + "?limit=2";
 
-function callback(res) {
-  if(res.statusCode != 200) {
-    console.log("unexpected status code " + res.statusCode);
+//parses a collection of Marc21 records into objects
+function parseMarc21XmlIntoObject(xmlString) {
+  var resultRecords = [];
+  var xmlTree = et.parse(xmlString);
+  //handle all records
+  xmlTree.findall("./record").forEach(function(record) {
+    var currentResultRecord = {};
+    //retrieve the values out of the controlfield elements
+    record.findall("./controlfield").forEach(function(field) {
+      var tag = field.get("tag");
+      currentResultRecord[tag] = field.text;
+    });
+    //retrieve the values out of the datafield elements
+    record.findall("./datafield").forEach(function(field) {
+      var tag = field.get("tag");
+      var datafieldContents = {};
+      field.findall("./subfield").forEach(function(subfield) {
+        var code = subfield.get("code");
+        datafieldContents[code] = subfield.text;
+      });
+      if(!(tag in currentResultRecord)) {
+        currentResultRecord[tag] = [];
+      }
+      currentResultRecord[tag].push(datafieldContents);
+    });
+    resultRecords.push(currentResultRecord);
+  });
+  return resultRecords;
+}
+
+//the callback which handles the answer from eXistDb
+function eXistCallback(xmlResults) {
+  if(xmlResults.statusCode != 200) {
+    console.error("unexpected status code " + xmlResults.statusCode);
   } else {
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log(chunk);
-    });
-    res.on('end', function(chunk) {
-      console.log("\n END OF RESPONSE")
-    });
+    xmlResults.setEncoding("utf8");
+    xmlResults.pipe(concat({encoding: "string"}, function(xmlString) {
+      var parsedData = parseMarc21XmlIntoObject(xmlString);
+      console.log(util.inspect(parsedData, null, 25));
+    }));
   }
 }
 
-var req = http.request(url, callback);
+//request the data from eXistDb
+var req = http.request(url, eXistCallback);
 req.on("error", function(e) {
-  console.log("error: " + e.message);
+  console.error("error: " + e.message);
 });
 req.end();
