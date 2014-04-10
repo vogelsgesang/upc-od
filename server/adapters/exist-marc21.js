aff125005e4f423a2fcdda0856d61b39eb3cbea7
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-04-10 04:26:31 +0200

    Marc21: moved creation of Xquery to NodeJS

--- server/adapters/exist-marc21.js
+++ server/adapters/exist-marc21.js
@@ -4,10 +4,6 @@ var http = require("http");
 var concat = require("concat-stream");
 var et = require("elementtree");
 
-var apiEndpoint = "http://localhost:8080/exist/rest//db/od/marc21_search.xq";
-
-var url =  apiEndpoint + "?limit=2";
-
 //parses a collection of Marc21 records into objects
 function parseMarc21XmlIntoObject(xmlString) {
   var resultRecords = [];
@@ -38,17 +34,76 @@ function parseMarc21XmlIntoObject(xmlString) {
   return resultRecords;
 }
 
+//builds an Xquery string in order to find the relevant records
+function buildMarc21Xquery(conditions, offset, limit) {
+  var selectionPaths = []; //set of Xpaths for retrieving the correct records
+  //handle empty condition arguments
+  if(conditions.length === 0) {
+    conditions = [[]];
+  }
+  //go through all the conditions (combined by an OR)
+  conditions.forEach(function(andConditions){
+    var conditionString = "";
+    //go through all the subconditions (combined by an AND)
+    andConditions.forEach(function(condition) {
+      var fieldName = condition[1];
+      var escapedValue = condition[2]
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/"/g, "&apos;");
+      if(condition[0] == "=") {
+        if(/^00[1-8]$/.test(fieldName)) {
+          conditionString += "[controlfield[@tag='"+fieldName+"']/text() = '"+escapedValue+"']";
+        } else if(/^[0-9]{3}[a-z0-9]$/.test(fieldName)) {
+          var tag = fieldName.substr(0,3);
+          var code = fieldName.substr(3,1);
+          conditionString += "[datafield[@tag='"+tag+"']/"+
+              "subfield[@code='"+code+"']/text() = '"+escapedValue+"']";
+        } else {
+          console.error("not supported...");
+        }
+      } else {
+        console.error("not supported...");
+      }
+    });
+    selectionPaths.push("/collection/record"+conditionString);
+  });
+  var selectionPath = "(\n   " + selectionPaths.join("\n | ") + "\n)";
+  var selectionXquery = "subsequence(" + selectionPath +","+offset+","+limit+")";
+  var namespaceDefinition = "declare default element namespace 'http://www.loc.gov/MARC21/slim';";
+  return namespaceDefinition + "\n" + selectionXquery;
+}
+
+var config = {
+  eXistEndpoint : "http://localhost:8080/exist/rest/",
+  xmlDocumentPath: "/db/od/books_export.xml",
+  limit: 20
+};
+
+var condition = [
+  [
+    ["=", "003", "SzGeCERN"],
+    ["=", "020a", "3540120351"]
+  ]
+];
+
+var xquery = buildMarc21Xquery(condition, 0, config.limit);
+console.log(xquery);
+
 //the callback which handles the answer from eXistDb
 function eXistCallback(xmlResults) {
   if(xmlResults.statusCode != 200) {
-    console.error("unexpected status code " + xmlResults.statusCode);
+    console.log("unexpected http status code " + xmlResults.statusCode);
   } else {
     xmlResults.setEncoding("utf8");
     xmlResults.pipe(concat({encoding: "string"}, function(xmlString) {
       var parsedCollection = parseMarc21XmlIntoObject(xmlString);
       var data = parsedCollection.map(function(record) {
         return {
-          _id: record["001"],
+          id: record["001"],
+          type: "marcRecord",
           fields: record
         }
       });
@@ -56,10 +111,28 @@ function eXistCallback(xmlResults) {
     }));
   }
 }
-
-//request the data from eXistDb
+//send the query...
+var url = config.eXistEndpoint + config.xmlDocumentPath + "?_query=" + encodeURIComponent(xquery);
+console.log(url);
 var req = http.request(url, eXistCallback);
 req.on("error", function(e) {
-  console.error("error: " + e.message);
+  errorCallback("requestFailed");
 });
 req.end();
+
+/*
+function resolveId(id, fields, successCallback, errorCallback) {
+  return queryExistForMarc21(eXistEndpoint + queryString, successCallback, errorCallback);
+};
+
+function query(objectType, conditions, successCallback, errorCallback) {
+  if(objectType != "marcRecord") {
+    errorCallback("unsupported object type" + objectType);
+    return function() {};
+  } else {
+    //TODO: build the query string
+    var queryString = "TODO";
+    return queryExistForMarc21(eXistEndpoint + queryString, successCallback, errorCallback);
+  }
+}
+*/
