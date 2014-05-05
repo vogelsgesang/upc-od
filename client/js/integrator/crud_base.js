3fc936848c7011eee6253e43f68b5c3a89708964
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-05-05 20:30:17 +0200

    client: extracted generic REST creator & editor

--- client/js/integrator/crud_base.js
+++ client/js/integrator/crud_base.js
@@ -19,17 +19,14 @@ function createBaucisResourceFactory(collectionName) {
 }
 
 /**
- * creates a new controller which displays an overview of a baucis collection
+ * creates a new controller which displays an overview of a baucis collection.
  * It expects a configuration as the first parameter with the following structure:
  * {
- *  'resourceName': the name of the registered factory function for this resource,
+ *  'resourceName': the name of the factory function for this resource,
  *  'scopeVariable': the name of the variable in the scope under which the data is saved,
  * }
  */
 function createOverviewController(config) {
-  if(Object.keys(config).indexOf('scopeVariable') < 0) {
-    config.scopeVariable = 'entries';
-  }
   return ['$document', '$scope', '$sce', '$q', '$alert', config.resourceName, function($document, $scope, $sce, $q, $alert, Resource) {
     function loadEntries() {
       $scope.state = "loading";
@@ -123,7 +120,7 @@ function createOverviewController(config) {
           }
           var msg = escapeStringForHtml(response.data.msg);
           if(response.status == 404) {
-            $scope[config['scopeVariable']] = $scope[config['scopeVariable']].filter(function(source) {
+            $scope[config.scopeVariable] = $scope[config.scopeVariable].filter(function(source) {
               return source._id != id;
             });
             severity = "warning";
@@ -139,3 +136,95 @@ function createOverviewController(config) {
   }];
 }
 
+/**
+ * creates a new controller for creating a new entry and storing it into a baucis collection.
+ * The first parameter is a configuration with the following structure:
+ * {
+ *  'resourceName': the name of the factory function for this resource,
+ *  'scopeVariable': the name of the variable in the scope under which the data is saved,
+ *  'initialValue': the initial value/the default values,
+ *  'transformForSave': this function is called before saving (1. param: the element to be saved)
+ *                      and the return value of it will be saved using baucis,
+ *  'afterSaveRedirection': the name of the site where the user should be forwarded to after
+ *                          the new entry was successfully saved
+ * }
+ */
+function createCreatorController(config) {
+  return ['$scope', '$alert', '$sce', '$location', config.resourceName, function($scope, $alert, $sce, $location, Resource) {
+    $scope[config.scopeVariable] = config.initialValue;
+    $scope.save = function() {
+      $scope.saving = true;
+      try {
+        var newObject = config.transformForSave(angular.copy($scope[config.scopeVariable]));
+      } catch(e) {
+        $alert({title: 'Error:', content: $sce.trustAsHtml(e.message), type: 'danger'});
+        $scope.saving = false;
+        return;
+      }
+      Resource.create(newObject, function() {
+        $scope.saving = false;
+        $location.path(config.afterSaveRedirection);
+      }, function(response) {
+        if(response && response.data && response.data.msg) {
+          var msg = escapeStringForHtml(response.data.msg);
+        } else {
+          msg = "Unknown error occured";
+        }
+        $alert({title: "Unable to save:", content: $sce.trustAsHtml(msg), type: 'danger'});
+        $scope.saving = false;
+      });
+    };
+  }];
+}
+
+/**
+ * creates a new controller which displays an overview of a baucis collection.
+ * The first parameter is a configuration with the following structure:
+ * {
+ *  'resourceName': the name of the factory function for this resource,
+ *  'scopeVariable': the name of the variable in the scope under which the data is saved,
+ *  'transformOnLoad': this function is called after loading the entry (1. param: the loaded entry)
+ *                     and the return value of it will be saved using baucis,
+ *  'transformForSave': this function is called before saving (1. param: the element to be saved)
+ *                      and the return value of it will be saved using baucis,
+ *  'afterSaveRedirection': the name of the site where the user should be forwarded to after
+ *                          the new entry was successfully saved
+ * }
+ */
+function createEditorController(config) {
+  return ['$scope', '$alert', '$sce', '$location', '$routeParams', config.resourceName, function($scope, $alert, $sce, $location, $routeParams, Resource) {
+    function loadSource() {
+      $scope.state = 'loading';
+      Resource.get({_id: $routeParams.id}, function(entry) {
+        $scope[config.scopeVariable] = config.transformOnLoad(entry);
+        $scope.state = 'ready';
+      }, function(result) {
+        $scope.state = 'error'
+      });
+    }
+    $scope.load = loadSource;
+    loadSource();
+    $scope.save = function() {
+      $scope.saving = true;
+      try {
+        var newValues = config.transformForSave(angular.copy($scope[config.scopeVariable]));
+      } catch(e) {
+        $alert({title: 'Error:', content: $sce.trustAsHtml(e.message), type: 'danger'});
+        $scope.saving = false;
+        return;
+      }
+      Resource.save(newValues, function() {
+        $scope.saving = false;
+        $location.path(config.afterSaveRedirection);
+      }, function(response) {
+        if(response && response.data && response.data.msg) {
+          var msg = escapeStringForHtml(response.data.msg);
+        } else {
+          msg = "Unknown error";
+        }
+        $alert({title: "Unable to save:", content: $sce.trustAsHtml(msg), type: 'danger'});
+        $scope.saving = false;
+      });
+    };
+  }];
+}
--- client/js/integrator/sources.js
+++ client/js/integrator/sources.js
@@ -1,102 +1,57 @@
 "use strict";
 (function(angular) {
-function escapeStringForHtml(string) {
-  return string
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
-}
-
-angular.module('Sources', ['ngResource'])
+angular.module('Sources', ['ngResource', 'mgcrea.ngStrap'])
 .factory('Sources', createBaucisResourceFactory('sources'))
 .controller('SourceOverview', createOverviewController({
   resourceName: 'Sources',
   scopeVariable: 'sources'
 }))
-.controller('SourceCreator', ['$scope', '$alert', '$sce', '$location', 'Sources', function($scope, $alert, $sce, $location, Sources) {
-  $scope.source = {};
-  $scope.source.adapter = {};
-  $scope.source.adapter.config = "{\n}";
-  $scope.source.mapping = "[\n]";
-  $scope.save = function() {
-    $scope.saving = true;
-    var sourceObject = angular.copy($scope.source);
+.controller('SourceCreator', createCreatorController({
+  resourceName: 'Sources',
+  scopeVariable: 'source',
+  initialValue: {'adapter':{'config':'{}'}, 'mapping': '[]'},
+  transformForSave: function(source) {
+    var source = angular.copy(source);
     try {
-      sourceObject.adapter.config = JSON.parse(sourceObject.adapter.config);
+      source.adapter.config = JSON.parse(source.adapter.config);
     } catch(e) {
-      $alert({title: "Error:", content: $sce.trustAsHtml("Adapter configuration of the source must be valid JSON"), type: 'danger'});
-      $scope.saving = false;
-      return;
+      throw new Error("Adapter configuration of the source must be valid JSON");
     }
     try {
-      sourceObject.mapping = JSON.parse(sourceObject.mapping);
+      source.mapping = JSON.parse(source.mapping);
     } catch(e) {
-      $alert({title: "Error:", content: $sce.trustAsHtml("Mapping must be valid JSON"), type: 'danger'});
-      $scope.saving = false;
-      return;
+      throw new Error("Mapping must be valid JSON");
     }
-    Sources.create(sourceObject, function() {
-      $scope.saving = false;
-      $location.path("/sources");
-    }, function(response) {
-      if(response && response.data && response.data.msg) {
-        var msg = escapeStringForHtml(response.data.msg);
-      } else {
-        msg = "Unknown error occured";
-      }
-      $alert({title: "Failed to save source", content: $sce.trustAsHtml(msg), type: 'danger'});
-      $scope.saving = false;
-    });
-  };
-}])
-.controller('SourceEditor', ['$scope', '$alert', '$sce', '$location', '$routeParams', 'Sources', function($scope, $alert, $sce, $location, $routeParams, Sources) {
-  $scope.state = 'loading';
-  function loadSource() {
-    Sources.get({_id: $routeParams.id}, function(source) {
-      if(source.adapter.config === undefined) {
-        source.adapter.config = {};
-      }
-      $scope.source = source;
-      $scope.state = 'ready';
-      $scope.source.adapter.config = JSON.stringify(source.adapter.config, null, 4);
-      $scope.source.mapping = JSON.stringify(source.mapping, null, 4);
-    }, function(result) {
-      $scope.state = 'error'
-    });
-  }
-  $scope.load = loadSource;
-  loadSource();
-  $scope.save = function() {
-    $scope.saving = true;
-    var sourceObject = angular.copy($scope.source);
+    return source;
+  },
+  afterSaveRedirection: '/sources'
+}))
+.controller('SourceEditor', createEditorController({
+  resourceName: 'Sources',
+  scopeVariable: 'source',
+  initialValue: {'adapter':{'config':'{}'}, 'mapping': '[]'},
+  transformOnLoad: function(source) {
+    if(source.adapter.config === undefined) {
+      source.adapter.config = {};
+    }
+    source.adapter.config = JSON.stringify(source.adapter.config, null, 4);
+    source.mapping = JSON.stringify(source.mapping, null, 4);
+    return source;
+  },
+  transformForSave: function(source) {
+    var source = angular.copy(source);
     try {
-      sourceObject.adapter.config = JSON.parse(sourceObject.adapter.config);
+      source.adapter.config = JSON.parse(source.adapter.config);
     } catch(e) {
-      $alert({title: "Error:", content: $sce.trustAsHtml("Adapter configuration of the source must be valid JSON"), type: 'danger'});
-      $scope.saving = false;
-      return;
+      throw new Error("Adapter configuration of the source must be valid JSON");
     }
     try {
-      sourceObject.mapping = JSON.parse(sourceObject.mapping);
+      source.mapping = JSON.parse(source.mapping);
     } catch(e) {
-      $alert({title: "Error:", content: $sce.trustAsHtml("Mapping must be valid JSON"), type: 'danger'});
-      $scope.saving = false;
-      return;
+      throw new Error("Mapping must be valid JSON");
     }
-    Sources.save(sourceObject, function() {
-      $scope.saving = false;
-      $location.path("/sources");
-    }, function(response) {
-      if(response && response.data && response.data.msg) {
-        var msg = escapeStringForHtml(response.data.msg);
-      } else {
-        msg = "Unknown error";
-      }
-      $alert({title: "Failed to save source", content: $sce.trustAsHtml(msg), type: 'danger'});
-      $scope.saving = false;
-    });
-  };
-}]);
+    return source;
+  },
+  afterSaveRedirection: '/sources'
+}));
 })(angular);
