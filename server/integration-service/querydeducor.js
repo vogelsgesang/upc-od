200568f38418fbea41d3cd7560a10048e87cb51c
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-05-30 15:22:35 +0200

    skeleton of querydeducor for Maryam

---
+++ server/integration-service/querydeducor.js
@@ -0,0 +1,25 @@
+function QueryDeducor(schemaDefinition) {
+  //deduces queries based on already known facts
+  //and on the schema definition
+  this.deduceQueries(facts) {
+    //you can access the schema definition using the variable schemaDefinition.
+    //I think that the equality definition schemaDefinition.equality is of special interest for you.
+    //The parameter facts will contain all the facts which are already known about a specific object
+    //(i.e. only the fields)
+    //
+    //Please return an array of query conditions which can be used in order to query for additional
+    //informations for a specific object. Return an array of the following format:
+    //[
+    //  [
+    //    ["=", "isbn", "1234"]
+    //  ],
+    //  [
+    //    ["=", "isbn", "23456"]
+    //  ]
+    //]
+    //This return value would have the meaning: search for all books with isbn 1234 or 23456
+    //
+    //for now, I just return an empty array. Just remove the following line as soon as you have something useful
+    return [];
+  }
+}
