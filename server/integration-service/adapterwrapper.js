b2eb78309817a94c9a0374b0adfc22bea48239e8
fbermeo82 <franz.bermeo@gmail.com>
2014-06-07 14:12:32 +0200

    wrapper with memcached code to manage cache in the queries

--- server/integration-service/adapterwrapper.js
+++ server/integration-service/adapterwrapper.js
@@ -1,7 +1,58 @@
 "use strict";
 var Mapper = require("./mapper");
 var Promise = require("bluebird");
+var MemCache = require("memcached");
 
+// Connect to the memcached server with its private AWS IP address
+var mcache = new MemCache('localhost:11211');
+
+/*
+ Function to identify is a object is Empty
+ */
+function isEmptyObject(obj) {
+  return !Object.keys(obj).length;
+}
+
+function KeyGenerator(conditions, fields) {
+  var key = "";
+  var sField = "";
+  var sConditions = "";
+  
+  if(isEmptyObject(conditions)){
+    if(isEmptyObject(fields)){
+      key = "ALL";
+    }
+    else {
+      var arrFields = fields.toString().split(",");
+      
+      arrFields = arrFields.sort();
+      sField = arrFields.join(",");
+      key = "F:" + sField;
+    }
+  }
+  else{
+    if(isEmptyObject(fields)){
+      var arrConditions = conditions.toString().split(",");
+      
+      arrConditions = arrConditions.sort();
+      sConditions = arrConditions.join(",");
+      key = "C:" + sConditions;
+    }
+    else {
+      var arrFields = fields.toString().split(",");
+      var arrConditions = conditions.toString().split(",");
+      
+      arrConditions = arrConditions.sort();
+      sConditions = arrConditions.join(",");
+      arrFields = arrFields.sort();
+      sField = arrFields.join(",");
+      
+      key = "C:" + sConditions + ";F:" + sField;
+    }
+  }
+  
+  return key;
+}
 /**
  * sets the configuration of the source with the according _id.
  * if no such source exists, it will be created.
@@ -33,16 +84,36 @@ function AdapterWrapper(sourceConfig) {
       objectType = relevantMapping["sourceType"];
       conditions = mapper.rewriteConditionsForSource(relevantMapping, conditions);
       fields = mapper.renameFieldsForSource(relevantMapping, fields);
+      var MCkey = "";
+      
+      MCkey = KeyGenerator(conditions, fields);
+      
       //check if it is in our cache?
       //if yes: send results from cache
       //else: send the query to the original source
       //send the query
-      abortFunction = adapter.query(objectType, conditions, fields, function successCallback(results) {
-        results = mapper.mapInstancesFromSource(results);
-        resolve(results);
-      }, function errorCallback(error) {
-        reject(error);
-      })
+      mcache.get(MCkey, function(err, result) {
+          if (!err) {
+            // Key found in cache, return value
+            console.log("Query have been found in cache: "+MCkey);
+            resolve(result[]);
+          }
+          else {
+              // Key not found, fetch value from ORIGINAL source
+              abortFunction = adapter.query(objectType, conditions, fields, function successCallback(results) {
+                results = mapper.mapInstancesFromSource(results);
+                
+                 mcache.set(MCkey, results, 7200, function(err, result){
+                  if(err) console.error(err);
+                  console.log("New query saved in cache: "+MCkey);
+                });
+                 
+                resolve(results);
+              }, function errorCallback(error) {
+                reject(error);
+              });
+          }
+      });
     }).cancellable().catch(Promise.CancellationError, function(e) {
       abortFunction();
       throw e; //Don't swallow the exception
