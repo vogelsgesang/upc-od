c61a819fa4dfa32b3e4491fbf8433cbc0298c551
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2014-06-12 20:16:05 +0200

    Fixed severe bugs in Memcache integration

--- server/integration-service/adapterwrapper.js
+++ server/integration-service/adapterwrapper.js
@@ -13,46 +13,49 @@ function isEmptyObject(obj) {
   return !Object.keys(obj).length;
 }
 
-function KeyGenerator(conditions, fields) {
-  var key = "";
-  var sField = "";
-  var sConditions = "";
-  
-  if(isEmptyObject(conditions)){
-    if(isEmptyObject(fields)){
-      key = "ALL";
+function KeyGenerator(sourceId) {
+  this.generateKey = function(type, conditions, fields) {
+    var key = type + ";";
+    var sField = "";
+    var sConditions = "";
+    
+    if(isEmptyObject(conditions)){
+      if(isEmptyObject(fields)){
+        key = "ALL";
+      }
+      else {
+        var arrFields = fields.toString().split(",");
+        
+        arrFields = arrFields.sort();
+        sField = arrFields.join(",");
+        key = "F:" + sField;
+      }
     }
-    else {
-      var arrFields = fields.toString().split(",");
-      
-      arrFields = arrFields.sort();
-      sField = arrFields.join(",");
-      key = "F:" + sField;
+    else{
+      if(isEmptyObject(fields)){
+        var arrConditions = conditions.toString().split(",");
+        
+        arrConditions = arrConditions.sort();
+        sConditions = arrConditions.join(",");
+        key = "C:" + sConditions;
+      }
+      else {
+        var arrFields = fields.toString().split(",");
+        var arrConditions = conditions.toString().split(",");
+        
+        arrConditions = arrConditions.sort();
+        sConditions = arrConditions.join(",");
+        arrFields = arrFields.sort();
+        sField = arrFields.join(",");
+        
+        key = "C:" + sConditions + ";F:" + sField;
+      }
     }
+    
+    return sourceId + ":" +key;
   }
-  else{
-    if(isEmptyObject(fields)){
-      var arrConditions = conditions.toString().split(",");
-      
-      arrConditions = arrConditions.sort();
-      sConditions = arrConditions.join(",");
-      key = "C:" + sConditions;
-    }
-    else {
-      var arrFields = fields.toString().split(",");
-      var arrConditions = conditions.toString().split(",");
-      
-      arrConditions = arrConditions.sort();
-      sConditions = arrConditions.join(",");
-      arrFields = arrFields.sort();
-      sField = arrFields.join(",");
-      
-      key = "C:" + sConditions + ";F:" + sField;
-    }
-  }
-  
-  return key;
 }
+
 /**
  * sets the configuration of the source with the according _id.
  * if no such source exists, it will be created.
@@ -62,6 +65,7 @@ function AdapterWrapper(sourceConfig) {
   var AdapterClass = require("../adapters/" + sourceConfig.adapter.name);
   var mapper = new Mapper(sourceConfig.mapping);
   var adapter = new AdapterClass(sourceConfig.adapter.config);
+  var keygenerator = new KeyGenerator(sourceConfig._id);
 
   /**
    * forwards the query to the corresponding adapter and
@@ -86,32 +90,32 @@ function AdapterWrapper(sourceConfig) {
       fields = mapper.renameFieldsForSource(relevantMapping, fields);
       var MCkey = "";
       
-      MCkey = KeyGenerator(conditions, fields);
+      MCkey = keygenerator.generateKey(objectType, conditions, fields);
       
       //check if it is in our cache?
       //if yes: send results from cache
       //else: send the query to the original source
       //send the query
       mcache.get(MCkey, function(err, result) {
-          if (!err) {
+          if (!err && result) {
             // Key found in cache, return value
-            console.log("Query have been found in cache: "+MCkey);
-            resolve(result[]);
+            console.log("Query has been found in cache: "+MCkey);
+            resolve(result);
           }
           else {
-              // Key not found, fetch value from ORIGINAL source
-              abortFunction = adapter.query(objectType, conditions, fields, function successCallback(results) {
-                results = mapper.mapInstancesFromSource(results);
-                
-                 mcache.set(MCkey, results, 7200, function(err, result){
-                  if(err) console.error(err);
-                  console.log("New query saved in cache: "+MCkey);
-                });
-                 
-                resolve(results);
-              }, function errorCallback(error) {
-                reject(error);
+            // Key not found, fetch value from ORIGINAL source
+            abortFunction = adapter.query(objectType, conditions, fields, function successCallback(results) {
+              results = mapper.mapInstancesFromSource(results);
+              
+              mcache.set(MCkey, results, 7200, function(err, result){
+                if(err) console.error(err);
+                else console.log("New query saved in cache: "+MCkey);
               });
+               
+              resolve(results);
+            }, function errorCallback(error) {
+              reject(error);
+            });
           }
       });
     }).cancellable().catch(Promise.CancellationError, function(e) {
