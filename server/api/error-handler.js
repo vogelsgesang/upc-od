vogelsgesang/upc-od

commit 1805c53fad9cbad3c489785d9d15847b7d8c3c67

API: better error reporting

server/api/error-handler.js
@@ -10,12 +10,14 @@ module.exports = function handleApiError(err, req, res, next) {
   if(err.statusCode !== undefined) {
     statusCode = err.statusCode;
   }
+  var response = {}
   var message = "An error occured";
   if(err.message !== undefined) {
-    message = err.message;
+    response.message = err.message;
+  }
+  if(err.stack) {
+    response.stack = err.stack;
   }
   res.statusCode = statusCode;
-  res.json({
-    msg: message
-  });
+  res.json(response);
 }

commit 2c6ce012b5f0ed15728c0520fcf81eab643adefe

API: fixed problem with error reporting
this bug was introduced in commit 1805c53fad9cbad3c489785d9d15847b7d8c3c67

server/api/error-handler.js
@@ -10,10 +10,9 @@ module.exports = function handleApiError(err, req, res, next) {
   if(err.statusCode !== undefined) {
     statusCode = err.statusCode;
   }
-  var response = {}
-  var message = "An error occured";
+  var response = {msg: "An error occurred"}
   if(err.message !== undefined) {
-    response.message = err.message;
+    response.msg = err.message;
   }
   if(err.stack) {
     response.stack = err.stack;